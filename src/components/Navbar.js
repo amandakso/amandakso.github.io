739bd73987964d1547d7b601e90bbf46ceb8cb24
Amanda So <soamanda95@gmail.com>
2022-11-09 15:14:51 -0800

    change css layout style of navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,18 @@
 import React from "react";
+import logo from "../assets/images/logo.png";
 
 const Navbar = () => {
   return (
-    <nav>
-      <ul>
-        <li>Home</li>
-        <li>About</li>
-        <li>Projects</li>
-        <li>Contact</li>
-      </ul>
+    <nav className="nav" id="navbar">
+      <div className="nav-content">
+        <img src={logo} width={80} height={80} alt="logo" id="nav-logo" />
+        <ul className="nav-items">
+          <li className="nav-item">Home</li>
+          <li className="nav-item">About</li>
+          <li className="nav-item">Projects</li>
+          <li className="nav-item">Contact</li>
+        </ul>
+      </div>
     </nav>
   );
 };
