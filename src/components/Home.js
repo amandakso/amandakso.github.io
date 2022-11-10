e39841cb6a474c720f1296ea41b97bcc178d81f2
Amanda So <soamanda95@gmail.com>
2022-11-10 15:38:33 -0800

    fix navbar to top of screen and remove max-width restrictions on elements

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Home = () => {
   return (
-    <div id="section1">
+    <div id="section1" className="section-content">
       <h1>Amanda So</h1>
       <p>Full stack web developer</p>
       <button>Learn More</button>
--- src/components/Section.js
+++ src/components/Section.js
@@ -4,7 +4,7 @@ export default function Section(props) {
   const data = props;
 
   return (
-    <section id={data.id}>
+    <section id={data.id} className="section-content">
       <h2>{data.title}</h2>
       <div>{data.content}</div>
     </section>
