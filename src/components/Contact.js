944fae75f92ae71718b42aa37b90a6efca912c69
Amanda So <soamanda95@gmail.com>
2022-11-09 20:41:43 -0800

    add links to contact icons

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,20 +3,32 @@ import mail from "../assets/images/email.png";
 
 export default function Contact() {
   return (
-    <div>
-      <img src={mail} alt="" />
-      <img
-        src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg"
-        height={36}
-        width={36}
-        alt=""
-      />
-      <img
-        src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/linkedin/linkedin-plain.svg"
-        height={36}
-        width={36}
-        alt=""
-      />
+    <div className="contact-container">
+      <div className="contact-item">
+        <a href="mailto:soamanda95@gmail.com">
+          <img src={mail} alt="" />
+        </a>
+      </div>
+      <div className="contact-item">
+        <a href="https://github.com/amandakso">
+          <img
+            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg"
+            height={36}
+            width={36}
+            alt=""
+          />
+        </a>
+      </div>
+      <div className="contact-item">
+        <a href="https://www.linkedin.com/in/amanda-so-412ba7217">
+          <img
+            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/linkedin/linkedin-plain.svg"
+            height={36}
+            width={36}
+            alt=""
+          />
+        </a>
+      </div>
     </div>
   );
 }
