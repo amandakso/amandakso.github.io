b1307c07f6e3f7f7e42122f5bdae09fcd3bd3983
Amanda So <soamanda95@gmail.com>
2022-11-09 16:52:54 -0800

    add css grid to projects section

--- src/components/Project.js
+++ src/components/Project.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Project(props) {
   const data = props;
   return (
-    <div>
+    <div className="project-container">
       <div>{data.photo}</div>
       <div>
         <h3>{data.title}</h3>
