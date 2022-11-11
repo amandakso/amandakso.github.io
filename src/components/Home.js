ca75d88d5c09db0821f2513439dbb5fa8f97c3ec
Amanda So <soamanda95@gmail.com>
2022-11-11 13:16:18 -0800

    add scroll function to Learn More button

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,23 @@
 import React from "react";
+import { Link } from "react-scroll";
 
 const Home = () => {
   return (
     <div id="section1" className="section-content home-container">
       <h1>Amanda So</h1>
       <p>Full stack web developer</p>
-      <button>Learn More</button>
+      <button>
+        <Link
+          activeClass="active"
+          to="section2"
+          spy={true}
+          smooth={true}
+          offset={-70}
+          duration={500}
+        >
+          Learn More
+        </Link>
+      </button>
     </div>
   );
 };
