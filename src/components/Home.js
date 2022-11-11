70082a1232e731bd1aec30e0e79a54881431fa2e
Amanda So <soamanda95@gmail.com>
2022-11-10 16:05:10 -0800

    change height of sections to fit the full screen

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Home = () => {
   return (
-    <div id="section1" className="section-content">
+    <div id="section1" className="section-content home-container">
       <h1>Amanda So</h1>
       <p>Full stack web developer</p>
       <button>Learn More</button>
