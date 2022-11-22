0d75b80d1857611e406df14282298fc45043bfe0
Amanda So <soamanda95@gmail.com>
2022-11-22 15:38:41 -0800

    adjust size of project screenshot images

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -12,9 +12,8 @@ const Projects = () => {
         id="project1"
         photo={
           <img
+            className="projectImg"
             src={project1}
-            width={250}
-            height={"auto"}
             alt="Screenshot of Project 1"
           />
         }
@@ -23,13 +22,12 @@ const Projects = () => {
       />
       <Project
         title="Project #2"
-        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut sapien elit, vehicula vitae facilisis bibendum, rutrum ut nunc. Curabitur eget augue sollicitudin, consequat enim vitae, cursus tellus. Proin mattis ipsum id magna efficitur, ac ornare sapien fringilla. Vivamus ullamcorper leo sapien, ultricies pharetra elit aliquam eget. Donec purus ex, faucibus eget consequat in, pharetra sed purus. Quisque at urna pretium, pulvinar ligula in, euismod eros. Aenean tempus tortor eget massa finibus, fermentum ultrices purus congue. Duis in elementum massa. Praesent quis sodales enim, ut fringilla tellus. Aliquam lobortis, tortor condimentum faucibus pulvinar, tellus leo venenatis ex, a mattis nisl diam et ex."
+        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut sapien elit, vehicula vitae facilisis bibendum, rutrum ut nunc. Curabitur eget augue sollicitudin, consequat enim vitae, cursus tellus. Proin mattis ipsum id magna efficitur, ac ornare sapien fringilla. Vivamus ullamcorper leo sapien, ultricies pharetra elit aliquam eget."
         id="project2"
         photo={
           <img
+            className="projectImg"
             src={project2}
-            width={250}
-            height={"auto"}
             alt="Screenshot of Project 2"
           />
         }
