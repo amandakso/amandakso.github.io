3fe446404262f283b55af6d4f34d137e3fe43d67
Amanda So <soamanda95@gmail.com>
2022-11-22 15:19:45 -0800

    add details for Pandagram (project1)

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,13 +1,14 @@
 import React from "react";
 import Project from "./Project";
-import project1 from "../assets/images/project1.jpg";
+import project1 from "../assets/images/project1.png";
+import project2 from "../assets/images/project2.jpg";
 
 const Projects = () => {
   return (
     <div>
       <Project
-        title="Project #1"
-        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut sapien elit, vehicula vitae facilisis bibendum, rutrum ut nunc. Curabitur eget augue sollicitudin, consequat enim vitae, cursus tellus. Proin mattis ipsum id magna efficitur, ac ornare sapien fringilla. Vivamus ullamcorper leo sapien, ultricies pharetra elit aliquam eget. Donec purus ex, faucibus eget consequat in, pharetra sed purus. Quisque at urna pretium, pulvinar ligula in, euismod eros. Aenean tempus tortor eget massa finibus, fermentum ultrices purus congue. Duis in elementum massa. Praesent quis sodales enim, ut fringilla tellus. Aliquam lobortis, tortor condimentum faucibus pulvinar, tellus leo venenatis ex, a mattis nisl diam et ex."
+        title="Pandagram"
+        description="An instagram clone application developed using React and Firebase. Users can upload photos, like/comment on posts, and follow other users."
         id="project1"
         photo={
           <img
@@ -15,6 +16,21 @@ const Projects = () => {
             width={250}
             height={"auto"}
             alt="Screenshot of Project 1"
+          />
+        }
+        demo="https://pandagram-39745.web.app/"
+        repo="https://github.com/amandakso/odin-instagram"
+      />
+      <Project
+        title="Project #2"
+        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut sapien elit, vehicula vitae facilisis bibendum, rutrum ut nunc. Curabitur eget augue sollicitudin, consequat enim vitae, cursus tellus. Proin mattis ipsum id magna efficitur, ac ornare sapien fringilla. Vivamus ullamcorper leo sapien, ultricies pharetra elit aliquam eget. Donec purus ex, faucibus eget consequat in, pharetra sed purus. Quisque at urna pretium, pulvinar ligula in, euismod eros. Aenean tempus tortor eget massa finibus, fermentum ultrices purus congue. Duis in elementum massa. Praesent quis sodales enim, ut fringilla tellus. Aliquam lobortis, tortor condimentum faucibus pulvinar, tellus leo venenatis ex, a mattis nisl diam et ex."
+        id="project2"
+        photo={
+          <img
+            src={project2}
+            width={250}
+            height={"auto"}
+            alt="Screenshot of Project 2"
           />
         }
         demo="#"
