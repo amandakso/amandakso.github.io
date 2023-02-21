9d660db4559abffef639536ee4be7592ea8952ae
Amanda So <soamanda95@gmail.com>
2023-02-20 21:49:38 -0800

    remove project 2 from projects section

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Project from "./Project";
 import project1 from "../assets/images/project1.png";
-import project2 from "../assets/images/project2.jpg";
+import project2 from "../assets/images/project2.png";
 
 const Projects = () => {
   return (
@@ -19,20 +19,6 @@ const Projects = () => {
         }
         demo="https://pandagram-39745.web.app/"
         repo="https://github.com/amandakso/odin-instagram"
-      />
-      <Project
-        title="Project #2"
-        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut sapien elit, vehicula vitae facilisis bibendum, rutrum ut nunc. Curabitur eget augue sollicitudin, consequat enim vitae, cursus tellus. Proin mattis ipsum id magna efficitur, ac ornare sapien fringilla. Vivamus ullamcorper leo sapien, ultricies pharetra elit aliquam eget."
-        id="project2"
-        photo={
-          <img
-            className="projectImg"
-            src={project2}
-            alt="Screenshot of Project 2"
-          />
-        }
-        demo="#"
-        repo="#"
       />
     </div>
   );
