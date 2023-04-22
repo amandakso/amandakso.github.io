7eeaf893146940c0e6c14fdc710be25def13d83d
Amanda So <soamanda95@gmail.com>
2023-04-21 19:41:22 -0700

    add project 2 details

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Project from "./Project";
 import project1 from "../assets/images/project1.png";
+import project2 from "../assets/images/project2.png";
 
 const Projects = () => {
   return (
@@ -18,6 +19,20 @@ const Projects = () => {
         }
         demo="https://pandagram-39745.web.app/"
         repo="https://github.com/amandakso/odin-instagram"
+      />
+      <Project
+        title="Blog Site"
+        description="A blog created with an API-only backend and two frontends. One frontend serves as the client-side blog site where users can access and comment on blog posts. The other frontend allows users to create and edit blog posts."
+        id="project2"
+        photo={
+          <img
+            className="projectImg"
+            src={project2}
+            alt="Screenshot of Project 2"
+          />
+        }
+        demo="https://amandakso.github.io/odin-blog-client/"
+        repo="https://github.com/amandakso/odin-blog-client"
       />
     </div>
   );
